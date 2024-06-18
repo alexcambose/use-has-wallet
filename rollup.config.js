fd2bdb7b3f7be48e2cc367db637d92a82ec12d61
alexcambose <alexcambose@yahoo.com>
2024-06-18 23:16:13 +0100

    Added useHasWalletExtension

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,12 @@
-const sourceMaps = require("rollup-plugin-sourcemaps")
-const packageJSON = require("./package.json")
+const sourceMaps = require('rollup-plugin-sourcemaps');
+const packageJSON = require('./package.json');
 export default {
-  input: "dist/index.js",
+  input: 'dist/index.js',
   output: {
-      file: packageJSON.main,
-      format: "umd",
-      name: "bundle.js",
-      sourcemap: true
-    }
-  ,
-  plugins: [
-    sourceMaps()
-  ]
-}
+    file: packageJSON.main,
+    format: 'umd',
+    name: 'bundle.js',
+    sourcemap: true,
+  },
+  plugins: [sourceMaps()],
+};
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,44 @@
-export function isBoolean(value: any): boolean {
-  return typeof value === "boolean";
-}
+import { useState, useEffect } from 'react';
+import { EIP6963ProviderDetail, createStore } from 'mipd';
 
-export default {
-  isBoolean,
+export const COINBASE_WALLET_RDNS = 'com.coinbase.wallet';
+export const COINBASE_SMART_WALLET_RDNS = 'coinbaseWalletSDK';
+export const METAMASK_RDNS = 'io.metamask';
+export const RAINBOW_RDNS = 'me.rainbow';
+export const WALLETCONNECT_RDNS = 'walletConnect';
+
+export const useHasWalletExtension = (rdns: string) => {
+  const [hasWalletExtension, setHasWalletExtension] = useState(false);
+
+  useEffect(() => {
+    const store = createStore();
+
+    const checkWalletExtension = (
+      providers: readonly EIP6963ProviderDetail<
+        any,
+        | 'io.metamask'
+        | 'com.coinbase'
+        | 'com.enkrypt'
+        | 'io.zerion'
+        | (string & {})
+      >[]
+    ) => {
+      const walletExtension = providers.find(
+        (provider) => provider.info.rdns === rdns
+      );
+      setHasWalletExtension(!!walletExtension);
+    };
+
+    store.subscribe(checkWalletExtension);
+
+    // Check initial state
+    const initialProviders = store.getProviders();
+    checkWalletExtension(initialProviders);
+
+    return () => {
+      store.destroy();
+    };
+  }, [rdns]);
+
+  return hasWalletExtension;
 };
